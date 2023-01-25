607a391b958ced89d1ee46b70e38581c5657d69f
AT-Alliance <atchezeu@groupealliance.eu>
2023-01-25 17:24:32 +0100

    Program java for jenkins

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args){
+			
+        for(int i=1;i<=10;i++){
+            
+            System.out.println("Hello world ......"+i);
+            
+        }
+        
+    }
+}
